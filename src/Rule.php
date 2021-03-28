7c3e7281648614b8a506afe7b21c62046f6de403
Daniel Simon <daniel.simon.91@outlook.com>
2021-03-28 23:01:18 +0200

    Created Rule entity

---
+++ src/Rule.php
@@ -0,0 +1,102 @@
+<?php declare(strict_types=1);
+
+namespace helmet91;
+
+class Rule
+{
+    private int $action;
+    private \DateInterval $actionDuration;
+    private int $repetitionCount;
+    private \DateInterval $evaluationPeriod;
+    private \DateInterval $cooldownPeriod;
+    private bool|array $splittable;
+    private array $dueAfter;
+
+    private function __construct() {}
+
+    public static function init() : self
+    {
+        return new self();
+    }
+
+    public function getAction() : int
+    {
+        return $this->action;
+    }
+
+    public function withAction(int $action) : self
+    {
+        $this->action = $action;
+
+        return $this;
+    }
+
+    public function getActionDuration() : \DateInterval
+    {
+        return $this->actionDuration;
+    }
+
+    public function withActionDuration(\DateInterval $duration) : self
+    {
+        $this->actionDuration = $duration;
+
+        return $this;
+    }
+
+    public function getRepetitionCount() : int
+    {
+        return $this->repetitionCount;
+    }
+
+    public function withRepetitionCount(int $count) : self
+    {
+        $this->repetitionCount = $count;
+        return $this;
+    }
+
+    public function getEvaluationPeriod() : \DateInterval
+    {
+        return $this->evaluationPeriod;
+    }
+
+    public function withEvaluationPeriod(\DateInterval $period) : self
+    {
+        $this->evaluationPeriod = $period;
+
+        return $this;
+    }
+
+    public function getCooldownPeriod() : \DateInterval
+    {
+        return $this->cooldownPeriod;
+    }
+
+    public function withCooldownPeriod(\DateInterval $period) : self
+    {
+        $this->cooldownPeriod = $period;
+
+        return $this;
+    }
+    public function getSplittable() : bool|array
+    {
+        return $this->splittable;
+    }
+
+    public function withSplittable(bool|array $splittable) : self
+    {
+        $this->splittable = $splittable;
+
+        return $this;
+    }
+
+    public function getDueAfter() : array
+    {
+        return $this->dueAfter;
+    }
+
+    public function withDueAfter(int $action, \DateInterval $period) : self
+    {
+        $this->dueAfter = ["action" => $action, "period" => $period];
+        return $this;
+    }
+}
---
+++ tests/RuleTest.php
@@ -0,0 +1,83 @@
+<?php declare(strict_types=1);
+
+namespace helmet91;
+
+use PHPUnit\Framework\TestCase;
+
+class RuleTest extends TestCase
+{
+    public function testCreatingRule() : void
+    {
+        $rule = Rule::init();
+
+        $this->assertInstanceOf(Rule::class, $rule);
+    }
+
+    /**
+     * @dataProvider providerTestAction
+     */
+    public function testAction($inputAction, $expectedAction) : void
+    {
+        $rule = Rule::init()->withAction($inputAction);
+
+        $this->assertEquals($expectedAction, $rule->getAction());
+    }
+
+    public function providerTestAction() : array
+    {
+        return [
+            [Action::RESTING, Action::RESTING],
+            [Action::DRIVING, Action::DRIVING],
+        ];
+    }
+
+    public function testActionDuration() : void
+    {
+        $rule = Rule::init()->withActionDuration(new \DateInterval("PT45M"));
+
+        $this->assertEquals(45, $rule->getActionDuration()->i);
+    }
+
+    public function testRepetitionCount() : void
+    {
+        $rule = Rule::init()->withRepetitionCount(3);
+
+        $this->assertEquals(3, $rule->getRepetitionCount());
+    }
+
+    public function testEvaluationPeriod() : void
+    {
+        $rule = Rule::init()->withEvaluationPeriod(new \DateInterval("P1W"));
+
+        $this->assertEquals(7, $rule->getEvaluationPeriod()->d);
+    }
+
+    public function testCooldownPeriod() : void
+    {
+        $rule = Rule::init()->withCooldownPeriod(new \DateInterval("P2W"));
+
+        $this->assertEquals(14, $rule->getCooldownPeriod()->d);
+    }
+
+    public function testSplittableAsBool() : void
+    {
+        $rule = Rule::init()->withSplittable(true);
+
+        $this->assertTrue($rule->getSplittable());
+    }
+
+    public function testSplittableAsArray() : void
+    {
+        $rule = Rule::init()->withSplittable([new \DateInterval("PT15M"), new \DateInterval("PT30M")]);
+
+        $this->assertEquals(30, $rule->getSplittable()[1]->i);
+    }
+
+    public function testDueAfter() : void
+    {
+        $rule = Rule::init()->withDueAfter(Action::DRIVING, new \DateInterval("P2D"));
+
+        $this->assertEquals(Action::DRIVING, $rule->getDueAfter()["action"]);
+        $this->assertEquals(2, $rule->getDueAfter()["period"]->d);
+    }
+}
